767db05a71560bae0f04edb34ef05184bccab7f1
Alexander <alexanderkronmikheev@gmail.com>
2022-09-24 03:55:23 +0300

    added first task

---
+++ hw01-gradle/src/main/java/ru/mikheev/HelloWorld.java
@@ -0,0 +1,11 @@
+package ru.mikheev;
+
+import com.google.common.base.Strings;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World isNullOrEmpty: " + Strings.isNullOrEmpty("Hello World"));
+    }
+}
